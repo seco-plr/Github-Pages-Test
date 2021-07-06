c9fc66a066937e3e8dc259c11ccacca8b33f3ed0
seco-plr <seco-plr@users.noreply.github.com>
2021-07-06 15:21:41 +0000

    Update available branches list

--- branches/feature/feature-abc/js/main.js
+++
@@ -1,1 +0,0 @@
-(new class{init(){document.addEventListener("DOMContentLoaded",this.onLoad)}onLoad(){console.log("Loaded")}}).init(),console.log("On branch: feature/feature-abc"),console.log("Welcome!");
